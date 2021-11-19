291272876fc6e7019e02ecda9d8eabf689f0fbb4
Abdhy Samoedra <abdhy_samoedra_28rpl@student.smktelkom-mlg.sch.id>
2021-11-19 08:30:49 +0700

    SplashScreen

--- app/src/main/java/com/android/example/mokletpay/SplashscreenActivity.java
+++ app/src/main/java/com/android/example/mokletpay/SplashscreenActivity.java
@@ -1,14 +1,29 @@
 package com.android.example.mokletpay;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashscreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splashscreen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 1000L); //3000 L = 3 detik
     }
 }
