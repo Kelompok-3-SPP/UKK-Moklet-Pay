c7ad67372287eed69960e06bcf421ffc0e886d2e
Rensi Meila Yulvinata <rensimeyla04@gmail.com>
2021-11-19 22:36:36 +0700

    Add LoginSiswaActivity

---
+++ app/src/main/java/com/android/example/mokletpay/siswa/LoginSiswaActivity.java
@@ -0,0 +1,16 @@
+package com.android.example.mokletpay.siswa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.example.mokletpay.R;
+
+public class LoginSiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_siswa);
+    }
+}
