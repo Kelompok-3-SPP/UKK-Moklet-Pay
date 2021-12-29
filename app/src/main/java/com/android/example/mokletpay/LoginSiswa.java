80af31b0ccfa10a77872cb0bfcb5bb06ee10739f
Rensi Meila Yulvinata <rensimeyla04@gmail.com>
2021-12-29 12:16:16 +0700

    Update LoginPetugas

--- app/src/main/java/com/android/example/mokletpay/LoginSiswa.java
+++ app/src/main/java/com/android/example/mokletpay/LoginSiswa.java
@@ -14,13 +14,5 @@ public class LoginSiswa extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_siswa);
 
-        Button btnLoginSiswa = findViewById(R.id.login_siswa);
-
-        btnLoginSiswa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginSiswa.this, HomeSiswa.class));
-            }
-        });
     }
 }
