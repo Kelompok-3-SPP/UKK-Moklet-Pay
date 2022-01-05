8b96049a0cc33a48668a625d18cedd47a1fe7cf3
SalsabillaMA <salsabilla_maurettasya_28rpl@student.smktelkom-mlg.sch.id>
2022-01-05 10:49:06 +0700

    update intent data

--- app/src/main/java/com/android/example/mokletpay/HomeAdmin.java
+++ app/src/main/java/com/android/example/mokletpay/HomeAdmin.java
@@ -1,8 +1,17 @@
 package com.android.example.mokletpay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.android.example.mokletpay.data.DataKelas;
+import com.android.example.mokletpay.data.DataPetugas;
+import com.android.example.mokletpay.data.DataSPP;
+import com.android.example.mokletpay.data.DataSiswa;
 
 public class HomeAdmin extends AppCompatActivity {
 
@@ -10,5 +19,37 @@ public class HomeAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_admin);
+
+        CardView cardSiswa = findViewById(R.id.datasiswa);
+        CardView cardKelas = findViewById(R.id.datakelas);
+        CardView cardSPP = findViewById(R.id.dataspp);
+        CardView cardPetugas = findViewById(R.id.datapetugas);
+
+        cardSiswa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeAdmin.this, DataSiswa.class));
+            }
+        });
+        cardKelas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeAdmin.this, DataKelas.class));
+            }
+        });
+        cardSPP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeAdmin.this, DataSPP.class));
+            }
+        });cardPetugas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeAdmin.this, DataPetugas.class));
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/android/example/mokletpay/MainActivity.java
+++ app/src/main/java/com/android/example/mokletpay/MainActivity.java
@@ -15,23 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton btnUser = findViewById(R.id.bguser);
-        ImageButton btnPetugas = findViewById(R.id.bgpetugas);
         ImageButton btnAdmin = findViewById(R.id.bgadmin);
-
-        btnUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginSiswa.class));
-            }
-        });
-
-        btnPetugas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginPetugas.class));
-            }
-        });
 
         btnAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
