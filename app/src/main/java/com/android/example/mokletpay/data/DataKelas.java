ea93ef3310bbb668b6c7b18be209fde4244b370c
tsoaprilia <tsoaprilia@gmail.com>
2022-01-06 08:59:07 +0700

    Revert "add adapter data kelas"
    This reverts commit cf48d370ac883e24e621553b24515aff56386379.

--- app/src/main/java/com/android/example/mokletpay/data/DataKelas.java
+++ app/src/main/java/com/android/example/mokletpay/data/DataKelas.java
@@ -1,38 +1,16 @@
 package com.android.example.mokletpay.data;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.android.example.mokletpay.R;
 
-import java.util.ArrayList;
-
 public class DataKelas extends AppCompatActivity {
-
-    private RecyclerView rvKelas;
-    private ArrayList<Kelas> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_kelas);
-
-        rvKelas = findViewById(R.id.recyclerHistory);
-        rvKelas.setHasFixedSize(true);
-
-        list.addAll(KelasData.getListDataKelas());
-        showRecyclerView();
     }
-
-    private void showRecyclerView() {
-        rvKelas.setLayoutManager(
-                new LinearLayoutManager(this));
-        ListKelasAdapter listKelasAdapter = new ListKelasAdapter(list);
-        rvKelas.setAdapter(listKelasAdapter);
-
-    }
-
 }
