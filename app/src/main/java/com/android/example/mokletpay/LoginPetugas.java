7101ba3b9d3ac8da4954f3377f6f1e640655ef33
tsoaprilia <tsoaprilia@gmail.com>
2022-01-17 10:03:47 +0700

    Revert "Merge branch 'home-siswa' into development"
    This reverts commit 8fb0a39c8c64269c68e3f3428c1a92171aeb0a8b, reversing
    changes made to 95c7083f286153c17e608370dd27ff1daa601c91.

--- app/src/main/java/com/android/example/mokletpay/LoginPetugas.java
+++ app/src/main/java/com/android/example/mokletpay/LoginPetugas.java
@@ -2,10 +2,7 @@ package com.android.example.mokletpay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class LoginPetugas extends AppCompatActivity {
 
--- app/src/main/java/com/android/example/mokletpay/MainActivity.java
+++ app/src/main/java/com/android/example/mokletpay/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.ImageButton;
+import com.android.example.mokletpay.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         btnUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginSiswa.class));
+                startActivity(new Intent(MainActivity.this, LoginUser.class));
             }
         });
 
